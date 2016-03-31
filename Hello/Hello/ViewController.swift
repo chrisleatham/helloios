cdbc8827e6bb4f367c2b986a18823cdb375b842b
Chris Leatham <chrisleatham@Chriss-MacBook-Pro.local>
2016-03-30 21:31:23 -0700

    Hello World

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Chris Leatham on 3/30/16.
+//  Copyright © 2016 Chris Leatham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
